52068e94449e61aef6bac9646a7863260acc7a05
Rithvik Patibandla <rithvikp98@gmail.com>
2018-12-05 09:45:47 +0530

    Minor improvements

--- 4.7-build-order.py
+++ 4.7-build-order.py
@@ -3,19 +3,17 @@ class Node:
         self.value=value
         self.dependencies=[]
 
-def dfs(i, nodes, depthMap):
+def dfs(i, nodes, heightMap):
     if visited[i]==2:
         raise Exception # loop present. Better to use custom exception class.
     visited[i]=2 # Used to detect loops
-    if visited[i]==1:
-        return
     if nodes[i].dependencies==[]:
         visited[i]=1
-        depthMap[i]=1
+        heightMap[i]=1
         return
     for j in nodes[i].dependencies:
-        dfs(j, nodes, depthMap)
-    depthMap[i]=max([depthMap[j] for j in nodes[i].dependencies])+1
+        dfs(j, nodes, heightMap)
+    heightMap[i]=max([heightMap[j] for j in nodes[i].dependencies])+1
     visited[i]=1
     
 n=int(input())
@@ -25,14 +23,14 @@ for i in range(int(input())):
     nodes[b].dependencies.append(a)
 
 visited=[0 for i in range(n)]
-depthMap=[0 for i in range(n)]
+heightMap=[0 for i in range(n)]
 
 for i in range(n):
     if visited[i]==1:
         continue
     else:
         try:
-            dfs(i, nodes, depthMap)
+            dfs(i, nodes, heightMap)
         except:
             b=1
 if b==1:
@@ -40,10 +38,10 @@ if b==1:
 else:
     order={}
     for i in range(n):
-        if depthMap[i] not in order:
-            order[depthMap[i]]=[i]
+        if heightMap[i] not in order:
+            order[heightMap[i]]=[i]
         else:
-            order[depthMap[i]].append(i)
+            order[heightMap[i]].append(i)
 
     for i in sorted(order.keys()):
         for j in order[i]:
